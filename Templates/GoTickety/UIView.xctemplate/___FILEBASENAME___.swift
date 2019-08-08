adb9f97d2be2d085bd73a3512319799f6fa74226
Maciej Królikowski <maciej.krolikowski@netguru.com>
2019-08-08 13:12:02 +0200

    Add uiview and uiviewcontroller to gotickety

---
+++ Templates/GoTickety/UIView.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,13 @@
+//___FILEHEADER___
+
+import Foundation
+
+class ___FILEBASENAME___: <#MyPlaceholder#> {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ Templates/GoTickety/UIViewController.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,10 @@
+//___FILEHEADER___
+
+import UIKit
+
+class ___FILEBASENAME___: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
