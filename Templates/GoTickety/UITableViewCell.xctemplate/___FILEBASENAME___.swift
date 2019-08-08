31e34bba13f4394a6e29e0ee2a798c2ed28429d2
Maciej Królikowski <maciej.krolikowski@netguru.com>
2019-08-08 13:29:35 +0200

    Add UITableViewCell and UITableViewController

---
+++ Templates/GoTickety/UITableViewCell.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,25 @@
+//___FILEHEADER___
+
+import UIKit
+
+final class ___FILEBASENAME___: BaseViewCell {
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension ___FILEBASENAME___: ViewSetupable {
+
+    func setupViewHierarchy() {
+
+    }
+
+    func setupConstraints() {
+
+    }
+}
---
+++ Templates/GoTickety/UITableViewController.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,18 @@
+//___FILEHEADER___
+
+import UIKit
+
+final class ___FILEBASENAME___: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
